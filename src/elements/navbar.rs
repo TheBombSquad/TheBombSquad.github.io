b377d6f3817d04352fd15db2c25d4d49f0050458
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2024-01-11 01:58:46 -0500

    Some tidying up/re-organization, update .gitignore, add Bootstrap CSS

--- src/elements/navbar.rs
+++ src/elements/navbar.rs
@@ -7,7 +7,7 @@ pub struct NavBarLink<'a> {
 }
 
 #[derive(Template, Default)]
-#[template(path = "navbar.html", escape="none")]
+#[template(path = "navbar.html", escape = "none")]
 pub struct NavigationBar<'a> {
     pub elements: Vec<NavBarLink<'a>>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
-mod elements;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 use askama::Template;
-use std::fs::{File, OpenOptions};
-use std::io::Write;
 
 use crate::elements::navbar::{NavBarLink, NavigationBar};
 
+mod elements;
+
 #[derive(Template)]
 #[template(path = "base.html")]
-struct HelloTemplate<'a> {
+struct BaseTemplate<'a> {
     title: &'a str,
     description: &'a str,
     navbar: NavigationBar<'a>,
 }
 
+fn create_navbar<'a>() -> NavigationBar<'a> {
+    let navbar_elements = vec![
+        NavBarLink { name: "Home", path: "" },
+        NavBarLink { name: "About", path: "/about.html" },
+    ];
+
+    NavigationBar { elements: navbar_elements }
+}
+
 fn main() {
-    let test_element = NavBarLink{name: "Title", path: "title.html"};
-    let test_navbar = NavigationBar{elements: vec![test_element, test_element]};
-    let test_template = HelloTemplate { title: "title!" , description: "description!", navbar: test_navbar };
-    let mut file = OpenOptions::new().create(true).write(true).truncate(true).open("out.html").unwrap();
-    file.write_all(test_template.render().unwrap().as_bytes()).unwrap();
+    let base = BaseTemplate { title: "title!", description: "description!", navbar: create_navbar() };
+    let mut file = OpenOptions::new().create(true).write(true).truncate(true).open("index.html").unwrap();
+    file.write_all(base.render().unwrap().as_bytes()).unwrap();
     file.flush().unwrap();
 }
