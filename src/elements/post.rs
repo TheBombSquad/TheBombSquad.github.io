2af796bc563987b966a201611101ddf282cc2822
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2025-11-05 00:58:49 -0500

    Changes to post formatting, add title/date parsing

--- src/elements/post.rs
+++ src/elements/post.rs
@@ -1,9 +1,11 @@
 use askama::Template;
+use chrono::NaiveDate;
 
 #[derive(Template, Default)]
 #[template(path = "post-body.html", escape = "none")]
 pub struct Post<'a> {
     pub title: &'a str,
     pub content: &'a str,
+    pub date: NaiveDate,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 use askama::Template;
-
+use chrono::{DateTime, Local, NaiveDate};
 use crate::elements::navbar::{NavBarLink, NavigationBar};
 use crate::elements::post::Post;
 
@@ -41,14 +41,19 @@ fn create_navbar<'a>() -> NavigationBar<'a> {
 fn main() {
     let navbar = create_navbar();
 
-    let post_content = markdown::to_html_with_options(&std::fs::read_to_string("posts/20251103.md").unwrap(),
-    &markdown::Options::gfm()).unwrap();
+    let post_metadata = std::fs::metadata("posts/20251103.md").unwrap();
+    let post_creation: DateTime<Local> = post_metadata.created().unwrap().into();
+    let post_date: NaiveDate = post_creation.date_naive();
+
+    let post_content_raw = &std::fs::read_to_string("posts/20251103.md").unwrap();
+    let post_title = &post_content_raw.lines().next().unwrap().trim();
+    let post_content = markdown::to_html_with_options(post_content_raw, &markdown::Options::gfm()).unwrap();
 
     let base = BaseTemplate {
         title: "Home",
         description: "Home page description goes here!",
         navbar,
-        post: Post { title: "Test", content: &post_content },
+        post: Post { title: &post_title, content: &post_content, date: post_date },
     };
 
     let mut file = OpenOptions::new()
